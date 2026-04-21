9e771d3d083afb488e127664f7cb26f6a6a6a62e
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-21 15:23:30 -0300

    build: insert AuthService scoped and DbContext configuration

--- ConsultaHub/Program.cs
+++ ConsultaHub/Program.cs
@@ -1,3 +1,7 @@
+using ConsultaHub.Data;
+using ConsultaHub.Services.Auth;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAuthentication();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

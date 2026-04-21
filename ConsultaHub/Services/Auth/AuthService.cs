f4775a2a2b0cca8803a8b5542f341329f3edac15
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-21 15:21:28 -0300

    feat: Authentication in service layer

---
+++ ConsultaHub/Services/Auth/AuthService.cs
@@ -0,0 +1,83 @@
+﻿using ConsultaHub.Data;
+using ConsultaHub.Models.Entities;
+using ConsultaHub.View.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ConsultaHub.Services.Auth
+{
+    public class AuthService(AppDbContext context, IConfiguration configuration) : IAuthService
+    {
+        public async Task<string?> LoginAsync(UserDto request)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+
+            var result = new PasswordHasher<User>()
+                .VerifyHashedPassword(user, user.PasswordHash, request.Password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return null;
+            }
+
+            return CreateToken(user);
+        }
+
+        public async Task<User> RegisterAsync(UserDto request)
+        {
+            if (await context.Users.AnyAsync(u => u.Username == request.Username))
+            {
+                return null;
+            }
+
+            var user = new User();
+            
+            var hassedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hassedPassword;
+
+            context.Users.Add(user);
+
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+        }
+    }
+}
---
+++ ConsultaHub/Services/Auth/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using ConsultaHub.Models.Entities;
+using ConsultaHub.View.DTOs;
+
+namespace ConsultaHub.Services.Auth
+{
+    public interface IAuthService
+    {
+        Task<User> RegisterAsync(UserDto request);
+        Task<string?> LoginAsync(UserDto request);
+    }
+}

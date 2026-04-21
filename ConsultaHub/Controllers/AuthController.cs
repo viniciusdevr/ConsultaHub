e61f8b2a8cb2a917fe83173a000603304643ae1c
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-21 12:23:40 -0300

    feat: create jwt maker function for authentication

--- ConsultaHub/Controllers/AuthController.cs
+++ ConsultaHub/Controllers/AuthController.cs
@@ -2,12 +2,16 @@
 using ConsultaHub.View.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ConsultaHub.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user { get; set; } = new();
 
@@ -33,8 +37,31 @@ namespace ConsultaHub.Controllers
             if (result == PasswordVerificationResult.Failed)
                 return BadRequest("Senha incorreta.");
 
-            string token = "success";
+            string token = CreateToken(user);
             return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
         }
     }
 }

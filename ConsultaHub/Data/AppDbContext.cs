766ba8a6cb95b78f4a9809e874e95d0e9e57b32d
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-21 15:19:44 -0300

    feat: setup db Connection with Entity FrameworkCore

---
+++ ConsultaHub/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ConsultaHub.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultaHub.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}

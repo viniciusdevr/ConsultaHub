545dee069969ad4cbcd34e946b74e63250c2caaf
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-26 11:12:00 -0300

    feat: add ClientFactory for Omie

--- ConsultaHub/Program.cs
+++ ConsultaHub/Program.cs
@@ -1,9 +1,12 @@
 using ConsultaHub.Data;
+using ConsultaHub.Models.Interfaces;
 using ConsultaHub.Services.Auth;
+using ConsultaHub.Services.Omie;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.IdentityModel.Tokens.Experimental;
+using System.Net.Http.Headers;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +32,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient<IClienteOmieApiClient, ClienteOmieApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://app.omie.com.br/api/v1/geral/clientes/");
+    client.Timeout = TimeSpan.FromSeconds(30);
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
+
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();

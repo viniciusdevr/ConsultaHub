5d54102b1fb722aaf6ce81b6a620a413c3f95fe3
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-26 11:12:30 -0300

    feat: add Omie controller

---
+++ ConsultaHub/Controllers/OmieController.cs
@@ -0,0 +1,29 @@
+﻿using ConsultaHub.Models.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultaHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OmieController(IClienteOmieApiClient clienteOmieApiClient, IConfiguration configuration) : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("clientesResumidos")]
+        public async Task<ActionResult> GetClientesOmie()
+        {
+            try
+            {
+                var appKey = configuration["Omie:AppKey"];
+                var appSecret = configuration["Omie:AppSecret"];
+                var clientes = await clienteOmieApiClient.GetClientesOmie(appKey, appSecret);
+                return Ok(clientes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro ao buscar clientes: {ex.Message}");
+            }
+        }
+    }
+}
